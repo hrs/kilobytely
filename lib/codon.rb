08cddb2f0880d3233fb96804760e23d6de05b81d
Harry Schwartz <hschwartz@brightcove.com>
2013-10-20 18:47:54 -0400

    tweak the hashing algorithm to create longer strings

--- lib/codon.rb
+++ lib/codon.rb
@@ -1,17 +1,25 @@
 module Codon
-  MAGIC_CODON_CODES = [4, 29, 6, 12, 9, 24, 14]
-  SIZE = MAGIC_CODON_CODES.size
+  SIZE = 14
 
+  # ensure that gcd(CHAR_OFFSET, BASE) == 1
+  CHAR_OFFSET = 25
+  BASE = 36
+
+  # map a single character to a string of `SIZE` random-looking
+  # characters.
   def char_to_codon(char)
-    MAGIC_CODON_CODES.map {|code|
-      offset_char = (code + char.to_i(36)) % 36
-      offset_char.to_s(36)
+    (1..SIZE).map { |mult|
+      offset = mult * CHAR_OFFSET
+      encoded_char = (offset + char.to_i(BASE)) % BASE
+      encoded_char.to_s(BASE)
     }.join
   end
 
+  # take a string of length `SIZE` and return the original character
+  # it was derived from
   def codon_to_char(codon)
-    # this code's really redundant, so the character can be derived
-    # from the first character of the codon
-    ((codon.chars.first.to_i(36) - MAGIC_CODON_CODES.first) % 36).to_s(36)
+    # the whole "code" is really redundant, so the original character
+    # can be derived from the first character of the codon
+    ((codon.chars.first.to_i(BASE) - CHAR_OFFSET) % BASE).to_s(BASE)
   end
 end
--- spec/lib/codon_spec.rb
+++ spec/lib/codon_spec.rb
@@ -6,19 +6,19 @@ describe Codon do
   it 'translates a base-36 character to a 6-character codon' do
     codon = char_to_codon('c')
 
-    expect(codon.to_s.size).to eq 7
-    expect(codon).to eq 'g5iol0q'
+    expect(codon.to_s.size).to eq 14
+    expect(codon).to eq '1qf4ti7wlazod2'
   end
 
   it 'translates integers' do
     codon = char_to_codon('3')
 
-    expect(codon.to_s.size).to eq 7
-    expect(codon).to eq '7w9fcrh'
+    expect(codon.to_s.size).to eq 14
+    expect(codon).to eq 'sh6vk9ync1qf4t'
   end
 
   it 'translates back from codons' do
-    expect(codon_to_char('g5iol0q')).to eq 'c'
-    expect(codon_to_char('7w9fcrh')).to eq '3'
+    expect(codon_to_char('1qf4ti7wlazod2')).to eq 'c'
+    expect(codon_to_char('sh6vk9ync1qf4t')).to eq '3'
   end
 end
